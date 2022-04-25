48c142ad363dc207acc6ad6919c2a728f710dfaf
Tatiana Kazakova <test.kazakova@gmail.com>
2022-04-25 21:55:49 +0300

    2 day

--- src/test/java/kzk/tati/SelenideInGithubSoftAssertions.java
+++ src/test/java/kzk/tati/SelenideInGithubSoftAssertions.java
@@ -3,6 +3,7 @@ package kzk.tati;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.CollectionCondition.itemWithText;
 import static com.codeborne.selenide.Condition.text;
@@ -16,11 +17,13 @@ public class SelenideInGithubSoftAssertions {
         $$("ul.repo-list li").first().$("[href]").click();
         $("#wiki-tab").click();
         $("div#wiki-body h1").shouldHave(text("Welcome to the selenide wiki!"));
-        $$("#wiki-pages-box li").find(Condition.text("SoftAssertions"));
-        $$(".markdown-body ul li").get(6).$("[href]").shouldHave(text("Soft assertions"));
-        $$(".markdown-body ul li").get(6).$("[href]").click();
+        $(".Box-row button").click();
+        $$("#wiki-pages-box li").find(Condition.text("SoftAssertions")).click();
+//        $$(".markdown-body ul li").get(6).$("[href]").shouldHave(text("Soft assertions"));
+//        $$(".markdown-body ul li").get(6).$("[href]").click();  Хочу это для себя оставить, как пример поиска элемента
         $(".gh-header").shouldHave(text("SoftAssertions"));
         $$(".markdown-body h4").shouldHave(itemWithText("3. Using JUnit5 extend test class:"));
+
 
 
     }
